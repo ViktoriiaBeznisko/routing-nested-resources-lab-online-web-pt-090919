3758564eb18ecc4303b4ac51665979ff4d4bf68b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-29 01:23:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists do
-  resources :songs, only: [:index, :show]  
-end
-  
+    resources :artists do
+    resources :songs, only: [:index, :show]  
+  end
   resources :songs
-  
-  
 end
